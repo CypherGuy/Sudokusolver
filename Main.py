e45570f7b01c2f569e918666a1e6e9c1d866f79c
CypherGuy <74025064+CypherGuy@users.noreply.github.com>
2021-04-15 16:20:20 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+#We define the board here. Remember to replace any gaps in the puzzle with 0's.
 board = [
      [0, 9, 0, 0, 0, 3, 6, 0, 0],
      [0, 0, 0, 1, 0, 0, 2, 0, 0],
@@ -8,17 +9,19 @@ board = [
      [2, 4, 0, 7, 0, 0, 5, 0, 3],
      [0, 0, 3, 0, 0, 2, 0, 0, 0],
      [0, 0, 8, 3, 0, 0, 0, 1, 0]
- ] #We define the board here
-def findEmpty(board):
+ ] 
+def findEmpty(board): #Function to find empty squares.
     for i in range(len(board)):
         for j in range(len(board[0])):
             if board[i][j] == 0:
                 return (i, j) #row, column
     return None
-def valid(board, num, pos):
+
+def valid(board, num, pos): #Function to determine if the number to place there is valid.
     row = pos[0] #X coordinate
     column = pos[1] #Y coordinate
-    #checking rows
+     
+    #checking rows for the same number
     for i in range(len(board[0])):
         if board[row][i] == num and column != i:
             return False
@@ -27,7 +30,7 @@ def valid(board, num, pos):
         if board[i][column] == num and row != i:
             return False
     
-    #checking box
+    #checking each box
     startRowBox = row//3 
     startColumnBox= column//3
     for i in range(startRowBox*3, (startRowBox*3)+3):
@@ -36,7 +39,7 @@ def valid(board, num, pos):
                 return False
     return True
 
-def printBoard(board):
+def printBoard(board): #This print the start and end product
     if not findEmpty(board):
         print("\nFinished puzzle\n")
     else:
@@ -51,17 +54,17 @@ def printBoard(board):
 
             
             print(str(board[i][j])+" ", end="")
-        print("\b") #Convert to bytes object, makes everything neater
+        print("\b") #Convert to bytes object, making everything neater
     
     
         
 def solve(board):
-    find = findEmpty(board)
+    find = findEmpty(board) #First we look for empty spaces.
     
     if not find:
         return True
     else:
-        row, col = find
+        row, col = find #We get the 'coordinates' of the box that's empty
     
     for i in range(1,10):
         if valid(board, i, find):
